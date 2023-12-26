c498665cfbcdc5a8e0aadd093b527ed958496def
Alperen Kişi <alperen@vision.tech>
2023-12-26 16:13:57 +0300

    refactor codestyling.

--- TMDB-RxSwift/TMDB-RxSwift/Scenes/SeriesList/PopularSeriesListViewController.swift
+++ TMDB-RxSwift/TMDB-RxSwift/Scenes/SeriesList/PopularSeriesListViewController.swift
@@ -9,69 +9,68 @@ import UIKit
 import RxSwift
 import RxCocoa
 
-class PopularSeriesListViewController: BaseViewController {
+final class PopularSeriesListViewController: BaseViewController {
     
-    let viewModel = PopularListViewModel()
-    
-    private var popularSeriesListCV: UICollectionView = {
+    private let viewModel = PopularListViewModel()
+    private let bag = DisposeBag()
+    private var popularSeriesListCollectionView: UICollectionView = {
         let flowLayout = UICollectionViewFlowLayout()
         flowLayout.itemSize = CGSize(width:screenWidth,
                                      height: 140)
         flowLayout.scrollDirection = UICollectionView.ScrollDirection.vertical
         flowLayout.minimumLineSpacing = 0
         
-        let cv = UICollectionView(frame: .zero,
+        let collectionView = UICollectionView(frame: .zero,
                                   collectionViewLayout: flowLayout)
-        cv.backgroundColor = UIColor.clear
-        cv.register(PopularSeriesListCVCell.self,
-                    forCellWithReuseIdentifier: "popularSeriesListCVCell")
-        return cv
+        collectionView.backgroundColor = UIColor.clear
+        collectionView.register(PopularSeriesListCollectionViewCell.self,
+                    forCellWithReuseIdentifier: PopularSeriesListCollectionViewCell.id)
+        return collectionView
     }()
-    
-    private var popularSeriesListCVPagining = false
-    private let bag = DisposeBag()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        title = "Populer Series"
+        title = "Popular Series"
         bindCollectionView()
     }
     
     override func initUI(){
         super.initUI()
-        mainView.addSubview(popularSeriesListCV)
-        popularSeriesListCV.snp.makeConstraints { (make) in
+        mainView.addSubview(popularSeriesListCollectionView)
+        popularSeriesListCollectionView.snp.makeConstraints { (make) in
             make.edges.equalToSuperview()
         }
     }
     
     func loadMoreData(){
-        makeLoader()
         viewModel.pageUp()
         viewModel.getData()
     }
     
     func bindCollectionView(){
-        viewModel.publishlist.bind(to: popularSeriesListCV.rx.items(cellIdentifier: "popularSeriesListCVCell",
-                                                                    cellType: PopularSeriesListCVCell.self)
-        ) { row,data,cell in
-            self.dismissLoader()
-            self.popularSeriesListCVPagining = false
-            cell.fillWith(movieName: data.name ?? "",
-                          movieShortDesc: data.overview ?? "",
-                          imageURL: API.shared.getImageURL(with: data.posterPath ?? ""),
-                          vote: data.voteAverage ?? 0)
+        viewModel.publishlist.bind(to: popularSeriesListCollectionView.rx.items(cellIdentifier: PopularSeriesListCollectionViewCell.id,
+                                                                                cellType: PopularSeriesListCollectionViewCell.self)) { _, data, cell in
+            let itemData = PopularSeriesItem(movieName: data.name ?? "",
+                                             movieShortDesc: data.overview ?? "",
+                                             imageURL: API.shared.getImageURL(with: data.posterPath ?? ""),
+                                             vote: data.voteAverage ?? 0)
+            cell.fillWith(item: itemData)
         }.disposed(by: bag)
         
-        handleScrollToLoadMoreSeries()
-        handleSeriesTapped()
+        popularSeriesListCollectionView.rx.willDisplayCell
+            .observe(on: MainScheduler.instance)
+            .map({ ($0.cell as? PopularSeriesListCollectionViewCell, $0.at.item) })
+            .subscribe { [weak self] cell, indexPath in
+                guard let self = self else { return }
+                if indexPath > self.viewModel.popularSeriesList.count - 5 {
+                    if self.viewModel.canLoadMore() {
+                        self.loadMoreData()
+                    }
+                }
+            }
+            .disposed(by: bag)
         
-        makeLoader()
-        viewModel.getData()
-    }
-    
-    func handleSeriesTapped(){
-        popularSeriesListCV.rx.modelSelected(PopularSeriesModel.self).bind { movie in
+        popularSeriesListCollectionView.rx.modelSelected(PopularSeriesModel.self).bind { movie in
             let detail = DetailViewController()
             detail.fillWith(image: movie.posterPath ?? "",
                             name: movie.name ?? "",
@@ -80,21 +79,7 @@ class PopularSeriesListViewController: BaseViewController {
                             vote: movie.voteAverage ?? 0)
             self.navigationController?.pushViewController(detail, animated: true)
         }.disposed(by: bag)
-    }
-    
-    func handleScrollToLoadMoreSeries(){
-        popularSeriesListCV.rx.willDisplayCell
-            .observe(on: MainScheduler.instance)
-            .map({ ($0.cell as? PopularSeriesListCVCell, $0.at.item) })
-            .subscribe { [weak self] cell, indexPath in
-                guard let self = self else { return }
-                if indexPath > self.viewModel.popularSeriesList.count - 5 {
-                    if self.viewModel.canLoadMore(),!self.popularSeriesListCVPagining {
-                        self.popularSeriesListCVPagining = true
-                        self.loadMoreData()
-                    }
-                }
-            }
-            .disposed(by: bag)
+        
+        viewModel.getData()
     }
 }
